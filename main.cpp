e2fa9cc52b373e24a88817b19b6022087cc23380
logbaby <96885996+loganbaby@users.noreply.github.com>
2022-03-21 18:00:30 +0300

    при добавлении С++ 17 filesystem работает успешно.

--- main.cpp
+++ main.cpp
@@ -1,18 +1,17 @@
-#define _SILENCE_EXPERIMENTAL_FILESYSTEM_DEPRECATION_WARNING
-#include <experimental/filesystem>
+#include <filesystem>
 #include <iostream>
 #include <string>
 #include <vector>
 
 int main() {
-  using std::experimental::filesystem::is_regular_file;
-  using std::experimental::filesystem::path;
-  using std::experimental::filesystem::recursive_directory_iterator;
+  using std::filesystem::is_regular_file;
+  using std::filesystem::path;
+  using std::filesystem::recursive_directory_iterator;
 
   auto recursiveGetFileNamesByExtension = [](path path,
                                              const std::string extension) {
     if (!is_regular_file(path)) {
-      std::vector<std::experimental::filesystem::path> paths;
+      std::vector<std::filesystem::path> paths;
       for (auto& i : recursive_directory_iterator(path)) {
         if (!i.path().extension().compare(extension)) {
           paths.push_back(i.path());
